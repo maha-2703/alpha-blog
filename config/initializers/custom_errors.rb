72fae4fd5ddf64136b321f3197139edc3378b2d0
maha <maha@example.com>
2026-04-24 06:59:28 +0530

    clean up layoyt

---
+++ config/initializers/custom_errors.rb
@@ -0,0 +1,5 @@
+
+
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag.html_safe
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   root "pages#home"
 get "about", to: "pages#about"
+resources :articles
 end
+
+
+
+
+
+
+
