be315e182bbfb1aa2a637f5c6fddbad38d2bc934
maha <maha@example.com>
2026-04-27 19:54:29 +0530

    adding signup form

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
-  resources :users
+  
   root "pages#home"
-get "about", to: "pages#about"
-resources :articles
+  get "about", to: "pages#about"
+  resources :articles
+
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+
+  
+
 end
 
 
