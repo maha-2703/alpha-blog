ca3871c8e8187c2832f671517a840e72555170fc
maha <maha@example.com>
2026-05-01 17:00:27 +0530

    add all changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,43 +1,71 @@
 class UsersController < ApplicationController
-    def show
-        @user = User.find(params[:id])
-        @articles = Article.all
-    end
 
-    def new
-        @user = User.new
-    end
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, only: [:edit, :update] 
+  before_action :require_same_user, only: [:edit, :update, :destroy] 
 
+  def index
+    @users = User.paginate(page: params[:page], per_page: 5)
+  end
+
+  def show
+  
+  
+  @articles = Article.paginate(page: params[:page], per_page: 5)
+
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = "Signup successful"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def edit
     
+  end
 
-    def edit 
-        @user = User.find(params[:id])
+  def update
+      if @user.update(user_params)
+      flash[:notice] = "Updated successfully"
+      redirect_to @user
+    else
+      render 'edit'
     end
+  end
 
-    def update
-        @user = User.find(params[:id])
-        if @user.update(user_params)
-            flash[:notice] = "your account information wast successfully updated"
-            redirect_to articles_path
-        else
-            render 'edit'
-        end
-    end
+  def destroy 
+    @user.destroy
+    session[:user_id] = nil if @user == current_user
+    flash[:notice] = "Account and all associated articles successfully deleted"
+    redirect_to articles_path
+  end
 
+  private
 
-    def create
-       @user = User.new(user_params)
-       if @user.save
-         flash[:notice] = "welcome to Alpha Blog #{@user.username}, you have successfully signup"
-         redirect_to articles_path
-       else
-        render 'new'
-       end
-    end
-    
-    private
-    def user_params
-        params.require(:user).permit(:username, :email, :password)
-    end
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+      @user = User.find(params[:id])
+  end 
+
+  def require_same_user
+    if current_user != @user && !current_user.admin?
+    flash[:alert] ="you can edit and update your own account"
+    redirect_to current_user
+    end 
+  end
+
 end
-     
